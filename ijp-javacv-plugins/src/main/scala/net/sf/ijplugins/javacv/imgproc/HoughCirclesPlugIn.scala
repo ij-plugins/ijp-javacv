3011ed7bbe16e0b91d6833e176a3330ece6eacd7
Jarek Sacha <jpsacha@gmail.com>
2019-09-28 14:06:09 -0400

    Add sample plugin for Hough circle detection

---
+++ ijp-javacv-plugins/src/main/scala/net/sf/ijplugins/javacv/imgproc/HoughCirclesPlugIn.scala
@@ -0,0 +1,176 @@
+/*
+ * Copyright (c) 2011-2019 Jarek Sacha. All Rights Reserved.
+ *
+ * Author's e-mail: jpsacha at gmail.com
+ */
+
+package net.sf.ijplugins.javacv.imgproc
+
+import java.awt.AWTEvent
+
+import ij.gui.{GenericDialog, OvalRoi, Overlay}
+import ij.plugin.filter.{PlugInFilter, PlugInFilterRunner}
+import ij.plugin.frame.RoiManager
+import ij.process.ImageProcessor
+import ij.{IJ, ImagePlus}
+import net.sf.ijplugins.javacv.IJOpenCVConverters._
+import net.sf.ijplugins.javacv.util.{ExtendedPlugInFilterTrait, IJPUtils, OpenCVUtils}
+import org.bytedeco.opencv.global.opencv_imgproc._
+import org.bytedeco.opencv.opencv_imgproc._
+
+object HoughCirclesPlugIn {
+  OpenCVUtils.initJavaCV()
+
+  private val method: Int = HOUGH_GRADIENT
+  private var dp: Double = 2
+  private var minDist: Double = 33
+  private var highThreshold: Double = 200
+  private var votes: Double = 100
+  private var minRadius: Int = 40
+  private var maxRadius: Int = 90
+  private var sendToResultsTable: Boolean = true
+}
+
+/**
+ * Finds lines in a binary image using the standard Hough transform.
+ *
+ * Input is a binary grey 8-bit image.
+ */
+class HoughCirclesPlugIn extends ExtendedPlugInFilterTrait {
+  _plugin =>
+
+  import HoughCirclesPlugIn._
+
+  override protected def Flags: Int = PlugInFilter.DOES_8G
+
+  override protected def Title = "OpenCV Hough Circles"
+
+  /**
+   * Method that does actual processing of the image.
+   *
+   * @param ip input image.
+   */
+  override protected def process(ip: ImageProcessor): Unit = {
+    val src = toMat(ip)
+
+    // Hough transform for circle detection
+    val circles = new Vec3fVector(0)
+    try {
+      HoughCircles(
+        src,
+        circles,
+        method,
+        dp,
+        minDist,
+        highThreshold,
+        votes,
+        minRadius,
+        maxRadius)
+    } catch {
+      case ex: RuntimeException =>
+        IJ.error(Title, "Error detecting Hough Circles.\n" + ex.getMessage)
+        throw ex
+    }
+
+    // Decode results as circle ROIs
+    try {
+      new RoiManager()
+      val roiManager = RoiManager.getInstance()
+
+      if (circles.size() > 0) {
+        val xyr = circlesToXYR(circles)
+
+        // Add circles to ROI Manager
+        roiManager.reset()
+
+        val circleRois = xyr.map { case (x, y, r) => new OvalRoi(x - r, y - r, 2 * r, 2 * r) }
+        circleRois.zipWithIndex.foreach { case (circleRoi, i) =>
+          circleRoi.setName(s"Circle ${i + 1}")
+          roiManager.addRoi(circleRoi)
+        }
+
+        // Show circles as an overlay on the current image
+        val overlay = new Overlay()
+        circleRois.foreach(overlay.addElement)
+        imp.setOverlay(overlay)
+
+        // TODO: Add to a Results Table
+        //      if (sendToResultsTable) {
+        //        val resultsTable = new ResultsTable()
+        //        val rhoTheta = circlesToRhoTheta(circles)
+        //        rhoTheta.zipWithIndex.foreach { case ((rho, theta), i) =>
+        //          resultsTable.incrementCounter()
+        //          resultsTable.addLabel(s"Circle ${i + 1}")
+        //          resultsTable.addValue("Rho", rho)
+        //          resultsTable.addValue("Theta", theta)
+        //        }
+        //        resultsTable.show(Title)
+        //      }
+      } else {
+        imp.setOverlay(null)
+        roiManager.reset()
+
+        // TODO: clear results table
+      }
+
+    }
+    catch {
+      case t: Throwable =>
+        t.printStackTrace()
+        throw t
+    }
+  }
+
+  override def showDialog(imp: ImagePlus, command: String, pfr: PlugInFilterRunner): Int = {
+
+    val message = "" +
+      "Finds circles in a grayscale image using the Hough transform."
+
+    val dialog = new GenericDialog(Title) {
+      addPanel(IJPUtils.createInfoPanel(Title, message))
+
+      addNumericField("DP", dp, 2, 10, "")
+      addNumericField("minDist", minDist, 2, 10, "pixels")
+      addNumericField("highThreshold", highThreshold, 2, 10, "")
+      addNumericField("votes", votes, 2, 10, "")
+      addNumericField("minRadius", minRadius, 2, 10, "pixels")
+      addNumericField("maxRadius", maxRadius, 2, 10, "pixels")
+      addCheckbox("Send to Results Table", sendToResultsTable)
+
+      addPreviewCheckbox(pfr)
+      addDialogListener(_plugin)
+      showDialog()
+    }
+
+    if (dialog.wasCanceled) {
+      return PlugInFilter.DONE
+    }
+
+    IJ.setupDialog(imp, Flags)
+  }
+
+  override def dialogItemChanged(gd: GenericDialog, e: AWTEvent): Boolean = {
+    //    private var method: Int = HOUGH_GRADIENT
+    dp = gd.getNextNumber
+    minDist = gd.getNextNumber
+    highThreshold = gd.getNextNumber
+    votes = gd.getNextNumber
+    minRadius = scala.math.round(gd.getNextNumber).toInt
+    maxRadius = scala.math.round(gd.getNextNumber).toInt
+
+    sendToResultsTable = gd.getNextBoolean
+
+    true
+  }
+
+  private def circlesToXYR(circles: Vec3fVector): Seq[(Float, Float, Float)] = {
+    for (i <- 0 until circles.size().toInt) yield {
+      val c = circles.get(i)
+      val x = c.get(0)
+      val y = c.get(1)
+      val r = c.get(2)
+
+      (x, y, r)
+    }
+  }
+}
