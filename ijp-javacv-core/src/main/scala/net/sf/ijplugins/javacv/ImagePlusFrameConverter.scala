d737e0ee8267aef7fd0e6fd8c22b693a4ec3fd27
Jarek Sacha <jpsacha@gmail.com>
2021-02-07 11:42:28 -0500

    Complete implementation of `ImagePlusFrameConverter`

--- ijp-javacv-core/src/main/scala/net/sf/ijplugins/javacv/ImagePlusFrameConverter.scala
+++ ijp-javacv-core/src/main/scala/net/sf/ijplugins/javacv/ImagePlusFrameConverter.scala
@@ -6,16 +6,16 @@
 
 package net.sf.ijplugins.javacv
 
-import java.nio.{ByteBuffer, FloatBuffer, ShortBuffer}
-
-import ij.ImagePlus
-import ij.process.{ColorProcessor, ImageProcessor}
+import ij.process.ImageProcessor
+import ij.{ImagePlus, ImageStack}
+import net.sf.ijplugins.javacv.ImageProcessorFrameConverter._
 import org.bytedeco.javacv.{Frame, FrameConverter}
 
 /**
- * Converts between ImagePlus and Frame.
+ * Converts between ImageJ's ImagePlus and JavaCV Frame.
  */
 class ImagePlusFrameConverter extends FrameConverter[ImagePlus] {
+  // TODO: Add option to assume that 3/4 channel images are color
 
   override def convert(imp: ImagePlus): Frame = toFrame(imp)
 
@@ -32,11 +32,11 @@ class ImagePlusFrameConverter extends FrameConverter[ImagePlus] {
       case ImagePlus.GRAY32 => (Frame.DEPTH_FLOAT, imp.getStackSize)
       case ImagePlus.COLOR_256 => (Frame.DEPTH_UBYTE, imp.getStackSize)
       case ImagePlus.COLOR_RGB =>
-        if (imp.getStackSize != 1) {
+        if (imp.getStackSize == 1) {
+          (Frame.DEPTH_UBYTE, 3)
+        } else {
           throw new UnsupportedOperationException(s"RGB ImagePlus supported only with stack size == 1, " +
             s"got: ${imp.getStackSize}")
-        } else {
-          (Frame.DEPTH_UBYTE, 3)
         }
       case t =>
         throw new UnsupportedOperationException(s"Unsupported ImagePlus type: $t")
@@ -47,157 +47,81 @@ class ImagePlusFrameConverter extends FrameConverter[ImagePlus] {
 
     if (frame == null ||
       frame.imageWidth != width ||
-      frame.imageHeight != imp.getHeight ||
+      frame.imageHeight != height ||
       frame.imageChannels != channels) {
       // Reallocate frame, its type or size changed
       frame = new Frame(width, height, imageDepth, channels)
     }
 
     // Copy pixels
-    // assume matching strides
-    val buffer = frame.image(0).position(0)
-
     val ips: Array[ImageProcessor] = {
       val stack = imp.getStack
       for (k <- (1 to stack.getSize).toArray) yield stack.getProcessor(k)
     }
 
-    imp.getType match {
-
-      case ImagePlus.GRAY8
-           | ImagePlus.COLOR_256 => // Byte
-        val srcPixels = ips.map(_.getPixels.asInstanceOf[Array[Byte]])
-        val dstPixels = new Array[Byte](frame.imageStride * height)
-        copyPixels(width, height, channels, frame.imageStride, srcPixels, dstPixels)
-
-        val dstBuffer = buffer.asInstanceOf[ByteBuffer]
-        dstBuffer.position(0)
-        dstBuffer.put(dstPixels)
-
-      case ImagePlus.GRAY16 => // Short
-        val srcPixels = ips.map(_.getPixels.asInstanceOf[Array[Short]])
-        val dstPixels = new Array[Short](frame.imageStride * height)
-        copyPixels(width, height, channels, frame.imageStride, srcPixels, dstPixels)
-
-        val dstBuffer = buffer.asInstanceOf[ShortBuffer]
-        dstBuffer.position(0)
-        dstBuffer.put(dstPixels)
-
-      case ImagePlus.GRAY32 => // Float
-        val srcPixels = ips.map(_.getPixels.asInstanceOf[Array[Float]])
-        val dstPixels = new Array[Float](frame.imageStride * height)
-        copyPixels(width, height, channels, frame.imageStride, srcPixels, dstPixels)
-
-        val dstBuffer = buffer.asInstanceOf[FloatBuffer]
-        dstBuffer.position(0)
-        dstBuffer.put(dstPixels)
-
-      case ImagePlus.COLOR_RGB =>
-        val dstBuffer = buffer.asInstanceOf[ByteBuffer]
-        // Only single slice color images are currently supported
-        val cp = ips(0).asInstanceOf[ColorProcessor]
-        val pixels = cp.getPixels.asInstanceOf[Array[Int]]
-        for (y <- 0 until height) {
-          val rowPixels = new Array[Byte](width * channels)
-          val yOffset = y * width
-          for (x <- 0 until width) {
-            val c: Int = pixels(yOffset + x)
-            //            val a = ((c & 0xff000000) >> 24).toByte
-            val r = ((c & 0xff0000) >> 16).toByte
-            val g = ((c & 0xff00) >> 8).toByte
-            val b = (c & 0xff).toByte
-            rowPixels(x * channels) = b
-            rowPixels(x * channels + 1) = g
-            rowPixels(x * channels + 2) = r
-            //            rowPixels(x * channels + 3) = a
-          }
-          dstBuffer.position(y * frame.imageStride)
-          dstBuffer.put(rowPixels)
-        }
-      case t =>
-        throw new UnsupportedOperationException(s"Unsupported ImagePlus type: $t")
-    }
+    copyPixels(ips, frame)
 
     frame
   }
 
-
-  private def copyPixels[T](width: Int,
-                            height: Int,
-                            channels: Int,
-                            imageStride: Int,
-                            srcPixels: Array[Array[T]],
-                            dstPixels: Array[T]): Unit = {
-    for (i <- 0 until height) {
-      val src_offset_i = i * width
-      val dst_offset_i = i * imageStride
-      if (channels == 1) {
-        // We can use a bit of speedup using system copy
-        System.arraycopy(srcPixels, src_offset_i, dstPixels, dst_offset_i, width)
-      } else {
-        for (j <- 0 until width) {
-          val dst_offset_j = dst_offset_i + j * channels
-          for (k <- 0 until channels) {
-            dstPixels(dst_offset_j + k) = srcPixels(k)(src_offset_i + j)
-          }
-        }
-      }
-    }
-  }
 
   private def toImagePlus(frame: Frame): ImagePlus = {
     if (frame == null || frame.image == null) {
       return null
     }
 
-    var alpha: Boolean = false
-    var offsets: Array[Int] = null
-    frame.imageChannels match {
-      case 1 =>
-        alpha = false
-        offsets = Array[Int](0)
-      case 3 =>
-        alpha = false
-        offsets = Array[Int](2, 1, 0)
-      case 4 =>
-        alpha = true
-        offsets = Array[Int](0, 1, 2, 3)
-      case _ =>
-        throw new RuntimeException("Unsupported `frame.imageChannels` = " + frame.imageChannels)
+    frame.imageDepth match {
+      case Frame.DEPTH_UBYTE => frame.imageChannels match {
+        case 3 => toRGB(frame)
+        case n => toGRAY8(frame)
+      }
+      case Frame.DEPTH_USHORT => toGRAY16(frame)
+      case Frame.DEPTH_FLOAT => toGRAY32(frame)
+      case imageDepth => throw new UnsupportedOperationException(s"Unsupported image depth: $imageDepth")
     }
 
-    //    var cm: ColorModel = null
-    //    var wr: WritableRaster = null
-    //    if (frame.imageDepth == Frame.DEPTH_UBYTE || frame.imageDepth == Frame.DEPTH_BYTE) {
-    //      cm = new ComponentColorModel(cs, alpha, false, Transparency.OPAQUE, DataBuffer.TYPE_BYTE)
-    //      wr = Raster.createWritableRaster(new ComponentSampleModel(DataBuffer.TYPE_BYTE, frame.imageWidth, frame.imageHeight, frame.imageChannels, frame.imageStride, offsets), null)
-    //    }
-    //    else if (frame.imageDepth == Frame.DEPTH_USHORT) {
-    //      cm = new ComponentColorModel(cs, alpha, false, Transparency.OPAQUE, DataBuffer.TYPE_USHORT)
-    //      wr = Raster.createWritableRaster(new ComponentSampleModel(DataBuffer.TYPE_USHORT, frame.imageWidth, frame.imageHeight, frame.imageChannels, frame.imageStride, offsets), null)
-    //    }
-    //    else if (frame.imageDepth == Frame.DEPTH_SHORT) {
-    //      cm = new ComponentColorModel(cs, alpha, false, Transparency.OPAQUE, DataBuffer.TYPE_SHORT)
-    //      wr = Raster.createWritableRaster(new ComponentSampleModel(DataBuffer.TYPE_SHORT, frame.imageWidth, frame.imageHeight, frame.imageChannels, frame.imageStride, offsets), null)
-    //    }
-    //    else if (frame.imageDepth == Frame.DEPTH_INT) {
-    //      cm = new ComponentColorModel(cs, alpha, false, Transparency.OPAQUE, DataBuffer.TYPE_INT)
-    //      wr = Raster.createWritableRaster(new ComponentSampleModel(DataBuffer.TYPE_INT, frame.imageWidth, frame.imageHeight, frame.imageChannels, frame.imageStride, offsets), null)
-    //    }
-    //    else if (frame.imageDepth == Frame.DEPTH_FLOAT) {
-    //      cm = new ComponentColorModel(cs, alpha, false, Transparency.OPAQUE, DataBuffer.TYPE_FLOAT)
-    //      wr = Raster.createWritableRaster(new ComponentSampleModel(DataBuffer.TYPE_FLOAT, frame.imageWidth, frame.imageHeight, frame.imageChannels, frame.imageStride, offsets), null)
-    //    }
-    //    else if (frame.imageDepth == Frame.DEPTH_DOUBLE) {
-    //      cm = new ComponentColorModel(cs, alpha, false, Transparency.OPAQUE, DataBuffer.TYPE_DOUBLE)
-    //      wr = Raster.createWritableRaster(new ComponentSampleModel(DataBuffer.TYPE_DOUBLE, frame.imageWidth, frame.imageHeight, frame.imageChannels, frame.imageStride, offsets), null)
-    //    }
-    //    else {
-    //      assert(false)
-    //    }
-    //
-    //    bufferedImage = new BufferedImage(cm, wr, false, null)
+  }
 
-    ???
+
+  private def toGRAY8(frame: Frame): ImagePlus = {
+    require(frame.imageDepth == Frame.DEPTH_UBYTE)
+
+    val dstPixels = copyPixelsByte(frame)
+
+    toImagePlus(frame.imageWidth, frame.imageHeight, dstPixels)
+  }
+
+
+  private def toGRAY16(frame: Frame): ImagePlus = {
+    require(frame.imageDepth == Frame.DEPTH_USHORT)
+
+    val dstPixels = copyPixelsShort(frame)
+
+    toImagePlus(frame.imageWidth, frame.imageHeight, dstPixels)
+  }
+
+
+  private def toGRAY32(frame: Frame): ImagePlus = {
+    require(frame.imageDepth == Frame.DEPTH_FLOAT)
+
+    val dstPixels = copyPixelsFloat(frame)
+
+    toImagePlus(frame.imageWidth, frame.imageHeight, dstPixels)
+  }
+
+
+  private def toRGB(frame: Frame): ImagePlus = {
+    val cp = toColorProcessor(frame)
+    new ImagePlus("", cp)
+  }
+
+
+  private def toImagePlus[T](width: Int, height: Int, pixels: Array[Array[T]]): ImagePlus = {
+    val stack = new ImageStack(width, height, pixels.length)
+    for (i <- pixels.indices) {
+      stack.setPixels(pixels(i), i + 1)
+    }
+
+    new ImagePlus("", stack)
   }
 }
--- ijp-javacv-core/src/test/scala/net/sf/ijplugins/javacv/ImagePlusFrameConverterTest.scala
+++ ijp-javacv-core/src/test/scala/net/sf/ijplugins/javacv/ImagePlusFrameConverterTest.scala
@@ -1,14 +1,17 @@
 package net.sf.ijplugins.javacv
 
-import ij.ImagePlus
 import ij.process.{ByteProcessor, ColorProcessor}
-import org.bytedeco.javacv.Frame
+import ij.{IJ, ImagePlus}
+import net.sf.ijplugins.javacv.util.OpenCVUtils.loadMulti
+import org.bytedeco.javacv.{Frame, OpenCVFrameConverter}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers._
 
+import java.io.File
+
 class ImagePlusFrameConverterTest extends AnyFlatSpec {
 
-  it should "convert GRAY8 toFrame" in {
+  it should "convert GRAY8 to Frame" in {
 
     val bp = new ByteProcessor(13, 17)
     val imp = new ImagePlus("", bp)
@@ -24,7 +27,7 @@ class ImagePlusFrameConverterTest extends AnyFlatSpec {
   }
 
 
-  it should "convert RGB toFrame" in {
+  it should "convert RGB to Frame" in {
 
     val cp = new ColorProcessor(13, 17)
     val imp = new ImagePlus("", cp)
@@ -39,4 +42,54 @@ class ImagePlusFrameConverterTest extends AnyFlatSpec {
     frame.imageChannels should be(3)
   }
 
+
+  it should "convert Frame to GRAY8" in {
+
+    val frame = new Frame(13, 17, Frame.DEPTH_UBYTE, 1)
+
+    val converter = new ImagePlusFrameConverter()
+
+    val imp = converter.convert(frame)
+
+    imp.getType should be(ImagePlus.GRAY8)
+    imp.getWidth should be(13)
+    imp.getHeight should be(17)
+    imp.getStackSize should be(1)
+  }
+
+
+  it should "convert Frame to GRAY8 Stack (from disk)" in {
+    val srcFile = new File("../test/data/mri-stack_c.tif").getCanonicalFile
+    assert(srcFile.exists())
+
+    val srcMat = loadMulti(srcFile)
+    srcMat should not be (null)
+
+    val openCVConverter = new OpenCVFrameConverter.ToMat()
+    val srcFrame = openCVConverter.convert(srcMat)
+    srcFrame should not be (null)
+
+    val converter = new ImagePlusFrameConverter()
+    val dstImp = converter.convert(srcFrame)
+    dstImp should not be (null)
+
+    dstImp.getType should be(ImagePlus.GRAY8)
+    dstImp.getWidth should be(186)
+    dstImp.getHeight should be(226)
+    dstImp.getStackSize should be(27)
+
+    // Read using ImageJ so we can compare conversion
+    val srcImp = IJ.openImage(srcFile.getCanonicalPath)
+    // Sanity checks
+    srcImp should not be (null)
+    srcImp.getWidth should be(186)
+    srcImp.getHeight should be(226)
+    srcImp.getStackSize should be(27)
+
+    val srcImageArray = srcImp.getStack.getImageArray
+    val dstImageArray = dstImp.getStack.getImageArray
+    for (i <- 0 until srcImp.getStackSize) {
+      srcImageArray(i) should be(dstImageArray(i))
+    }
+  }
 }
